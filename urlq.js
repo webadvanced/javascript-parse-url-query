b3f0b0e2bbcf442abd9b8c9b7fdc0afcf4093b83
Web Advanced <paul.barton@webadvanced.com>
2013-06-05 17:01:06 -0700

    initial load

---
+++ urlq.js
@@ -0,0 +1,37 @@
+( function( w, q, undefined ) {
+	
+	"use strict";
+	
+	var	qObject = {},
+		qCollection,
+		i = 0,
+		urlq = {};
+
+	//If there is no query, exit
+	if( !q ) {
+		return;
+	}
+
+	//build quiry object
+	qCollection = q.slice( 1 ).split( '&' );
+	for( ; i < qCollection; i ++ ) {
+		var entry = qCollection[ i ].split( '=' );
+		qObject[ entry[ 0 ] ] = entry[ 1 ];
+	}
+	//count of query params
+	urlq.count = qCollection.length;
+
+	//get value from key
+	urlq.get = function( key ) {
+		return qObject[ key ];
+	};
+
+	//exists by key
+	urlq.exists = function( key ) {
+		return qObject[ key ] !== undefined;
+	};
+
+	//give it to the people
+	w.urlq = urlq;
+
+}( window, window.location.query ) );
